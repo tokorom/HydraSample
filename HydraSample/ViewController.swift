a8de47b395b3b4401930a819d49e44be22bd1458
tokorom <tokorom@gmail.com>
2019-03-08 16:06:36 +0900

    First commit

---
+++ HydraSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HydraSample
+//
+//  Created by tokorom on 2019/03/08.
+//  Copyright © 2019 Spinners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
